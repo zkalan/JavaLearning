586f50d5ebe7480103b728c218b5fa8a52d36d42
zkalan <zkalan9@gmail.com>
2019-08-11 17:37:32 +0800

    生成五个不同的随机数

--- StringTools.java
+++ StringTools.java
@@ -1,5 +1,22 @@
+
+import java.util.Random;
+
 public class StringTools{
     public static void main(String[] args){
-        
+        Random r = new Random();
+        int[] result = {0,0,0,0,0};
+        int i = -1;
+        int num = 0;
+        int times = 0;
+        while(num < 5){
+            i = r.nextInt(5);
+            if(result[i] == 0){
+                result[i] = i;
+                num++;
+                System.out.println(i);
+            }
+            times++;
+        }
+        System.out.println("--------------------"+times+"-----------------");
     }
 }
